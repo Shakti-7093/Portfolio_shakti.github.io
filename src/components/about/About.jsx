ee014a87020071229245afb508b8fdd41c1c448a
shakti7093 <145318489+Shakti-7093@users.noreply.github.com>
2023-11-22 17:01:09 +0530

    Add files via upload

---
+++ src/components/about/About.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import "./About.css";
+import { FaAward } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { VscFolderLibrary } from "react-icons/vsc";
+
+function About() {
+  return (
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+      <div className="container about_container">
+        <div className="about_me">
+          <div className="about_me-image">
+            <img src="https://i.pinimg.com/564x/ea/60/d0/ea60d0bedf39ec7057c29bb7cc4e99b0.jpg" alt="" />
+          </div>
+        </div>
+        <div className="about_content">
+          <div className="about_cards">
+            <article className="about_card">
+              <FaAward className="about_icon" />
+              <h5>Experirnce</h5>
+              <small>1+ Years Working</small>
+            </article>
+            <article className="about_card">
+              <FiUsers className="about_icon" />
+              <h5>Clients</h5>
+              <small>20+ Worldwide</small>
+            </article>
+            <article className="about_card">
+              <VscFolderLibrary className="about_icon" />
+              <h5>Projects</h5>
+              <small>45+ completed</small>
+            </article>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime eius
+            nihil cupiditate laborum consequuntur, dicta hic consectetur libero
+            ipsum corrupti rem dignissimos, facere velit sit omnis
+            exercitationem error magnam doloribus.
+          </p>
+
+          <a href="#contact" className="btn btn-primary">Let's Talk</a>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default About;
